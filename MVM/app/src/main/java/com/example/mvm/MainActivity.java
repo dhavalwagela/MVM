ccf6774dbe84373d48ed285df0836fb58e8881a8
Dhaval Wagela <dhaval.wagela@hotwaxsystems.com>
2020-02-28 03:21:30 -0600

    Implemented: 'Show password' functionality while typing the password at the time of logging in (BranchI)

--- MVM/app/src/main/java/com/example/mvm/MainActivity.java
+++ MVM/app/src/main/java/com/example/mvm/MainActivity.java
@@ -1,31 +1,50 @@
 package com.example.mvm;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.LinkMovementMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.AppCompatCheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
     /*TODO Need to make fields Mandatory*/
     TextView textView;
     EditText username, password;
+
+    EditText edtPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textViewLink);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+        @SuppressLint("WrongViewCast") AppCompatCheckBox checkbox = (AppCompatCheckBox) findViewById(R.id.checkBox);
+        edtPassword = findViewById(R.id.pwdText);
+            checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                    if (isChecked) {
+                        edtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    } else {
+                        edtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    }
+                }
+            });
     }
     public void checkValidUser(View view) {
-        username = (EditText) findViewById(R.id.userText);
-        password = (EditText) findViewById(R.id.pwdText);
+        username = findViewById(R.id.userText);
+        password = findViewById(R.id.pwdText);
         if (username.getText().toString().trim().length() != 0 && password.getText().toString().trim().length() != 0) {
             SQLiteDatabase sqldb = this.openOrCreateDatabase("VendingVehicleMachine.db", MODE_PRIVATE, null);
             Cursor cursor = sqldb.rawQuery("select name FROM sqlite_master WHERE type='table' AND name='tbl_registerUser'", null);
