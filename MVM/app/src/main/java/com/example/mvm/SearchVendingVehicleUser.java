d4a3ed961f48a522041f97c33dfb8e6abe0072e4
Dhaval Wagela <dhaval.wagela@hotwaxsystems.com>
2020-03-29 18:56:20 -0500

    Finalized changes for screenshots (BranchI)

--- MVM/app/src/main/java/com/example/mvm/SearchVendingVehicleUser.java
+++ MVM/app/src/main/java/com/example/mvm/SearchVendingVehicleUser.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -60,17 +62,15 @@ public class SearchVendingVehicleUser extends AppCompatActivity implements Adapt
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_vending_vehicle_user);
-        TextView t1 = (TextView) findViewById(R.id.text1);
-        t1.setMovementMethod(LinkMovementMethod.getInstance());
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         Spinner spinner_next = (Spinner) findViewById(R.id.spinner2);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.location_name, android.R.layout.simple_spinner_item);
+                R.array.vehicle_name, android.R.layout.simple_spinner_item);
 
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.vehicle_name, android.R.layout.simple_spinner_item);
+                R.array.location_name, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
